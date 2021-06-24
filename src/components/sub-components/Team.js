264da42d4e58bebd83bf2f415d7a8173efa69079
kevin bhavsar <kevinbhavsar567@gmail.com>
2021-06-24 16:10:14 +0530

    add new team member

--- src/components/sub-components/Team.js
+++ src/components/sub-components/Team.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "../../styles/sub-components/Team.scss";
 import CEOImage from "../../assets/ceo.svg";
 import CTOImage from "../../assets/cto.svg";
+import Director from "../../assets/director.svg";
 import DirectorImage from "../../assets/director.jpeg";
 export default class Team extends Component {
   componentDidMount() {
@@ -13,14 +14,16 @@ export default class Team extends Component {
   render() {
     return (
       <div className="px-4 py-2">
-        <h2 class="about-h2">Our Team</h2>
+        <h2 class="about-h2" id="team-heading">
+          Our Team
+        </h2>
         <div className="team-row">
           <div className="team-col">
             <div
               data-tilt
               data-tilt-glare
               data-tilt-max-glare="0.8"
-              className="team-card px-3 py-4"
+              className="team-card px-1 py-2"
             >
               <img src={CEOImage} className="team-image" alt="" />
               <p className="p-big bold">Jonathan Emmanuel</p>
@@ -32,19 +35,20 @@ export default class Team extends Component {
               data-tilt
               data-tilt-glare
               data-tilt-max-glare="0.8"
-              className="team-card px-3 py-4"
+              className="team-card px-2 py-2"
             >
               <img src={CTOImage} className="team-image" alt="" />
               <p className="p-big bold">Rajesh Chinnadurai</p>
               <p className="text-secondary m-0">Co-Founder / CTO</p>
             </div>
           </div>
+
           <div className="team-col">
             <div
               data-tilt
               data-tilt-glare
               data-tilt-max-glare="0.8"
-              className="team-card px-3 py-4"
+              className="team-card px-2 py-2"
             >
               <img
                 id="dir-img"
@@ -53,6 +57,18 @@ export default class Team extends Component {
                 alt=""
               />
               <p className="p-big bold">Carlie Jayne Guy</p>
+              <p className="text-secondary m-0">Director</p>
+            </div>
+          </div>
+          <div className="team-col">
+            <div
+              data-tilt
+              data-tilt-glare
+              data-tilt-max-glare="0.8"
+              className="team-card px-2 py-2"
+            >
+              <img src={Director} className="team-image" alt="" />
+              <p className="p-big bold">Jerome Jones</p>
               <p className="text-secondary m-0">Director</p>
             </div>
           </div>
